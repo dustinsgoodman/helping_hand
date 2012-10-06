7cbdbc21e35f16f70e1ad74968f5fe1ec3359156
Peter <parkper@gmail.com>
2012-10-05 22:43:37 -0400

    Add to user controller

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,6 +1,33 @@
 class UserController < ApplicationController
   def index
+    @user = User.find(params[:id])
+  end
+
+  def new
     @user = User.new
+  end
+
+  def update
+
+  end
+
+  def create
+    @user = User.create(params)
+    if @user.save
+      flash[:notice] = "User created succesfully"
+      render :
+    else
+
+    end
+  end
+
+  def destroy
+    @user.destroy
+    redirect_to user_path
+  end
+
+  def show
+
   end
 
 end
