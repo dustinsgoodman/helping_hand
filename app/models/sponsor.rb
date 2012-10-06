6fd8e6a6984b2981ca6fc899c9a0eb668bb4e33f
Peter <parkper@gmail.com>
2012-10-06 11:43:31 -0400

    Added sponsor model

--- app/models/sponsor.rb
+++ app/models/sponsor.rb
@@ -1,3 +1,28 @@
 class Sponsor < ActiveRecord::Base
   has_and_belongs_to_many :opportunites
+
+  attr_accessible :name, :email, :rating
+
+  validates_presence_of :name
+
+  def self.find_or_create(attrs)
+    email = attrs[:email]
+    user = nil
+    if email
+      begin
+        connection.execute "LOCK TABLES users WRITE"
+        sponsor = find(:first, :conditions => {:email => email})
+        unless sponsor
+          logger.info "Organization #{email} not found, creating"
+           = create(attrs)
+        end
+      ensure
+        connection.execute "UNLOCK TABLES"
+      end
+      user
+    else
+      logger.error "Must specify email when creating user"
+      nil
+    end
+  end
 end
