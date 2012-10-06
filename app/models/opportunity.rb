239de7af80c6b09a4dc2991e7d0d969072bf9d82
Peter <parkper@gmail.com>
2012-10-06 18:50:19 -0400

    Peters last commit

--- app/models/opportunity.rb
+++ app/models/opportunity.rb
@@ -1,6 +1,5 @@
 class Opportunity < ActiveRecord::Base
   has_and_belongs_to_many :users
-  has_one :location
-  
+
   attr_accessible :name, :description, :max_ppl, :min_ppl, :event_start, :event_end, :location_id, :owner_id, :num_ppl
 end
