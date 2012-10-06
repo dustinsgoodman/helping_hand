b78e6097107a1712fa92306f5db147f0c4b1ffaf
Peter <parkper@gmail.com>
2012-10-06 12:12:24 -0400

    Added location model

--- app/models/location.rb
+++ app/models/location.rb
@@ -2,5 +2,25 @@ class Location < ActiveRecord::Base
   has_and_belongs_to_many :opportunites
   has_and_belongs_to_many :users
 
+  def self.find_or_create(attrs)
+    city = attrs[:city]
 
+
+    if location
+      begin
+        connection.execute "LOCK TABLES users WRITE"
+        location = location = Location.find(attrs[:name])
+        unless location
+          logger.info "location #{email} not found, creating"
+          location = create(attrs)
+        end
+      ensure
+        connection.execute "UNLOCK TABLES"
+      end
+      location
+    else
+      logger.error "Must specify email when creating user"
+      nil
+    end
+  end
 end
