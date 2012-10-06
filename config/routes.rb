27aac6c967d0707badae669f0b7dd096bd38dda9
Dustin Goodman <dustin.goodman@clickscape.com>
2012-10-06 09:44:02 -0400

    updated routes for user sessions

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,13 @@ HelpingHand::Application.routes.draw do
   resources :users,
     :controller => :users
 
+  resource :user_session, :only => [:new, :create, :destroy]
+  with_options :controller => "user_sessions" do |sess|
+    sess.get '/login', :action => 'new'
+    sess.post '/logout', :action => 'destroy',
+      :conditions => {:method => :delete}
+  end
+  
   resources :opportunities,
     :controller => :opportunities
 end
